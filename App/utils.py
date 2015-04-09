8ce30f45abd6b26ae38b1e76524d8d919e608667
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-04-09 23:39:37 +0530

    demo addition done but 405 error

--- App/utils.py
+++ App/utils.py
@@ -5,13 +5,14 @@
 from pymongo import MongoClient
 from config import config
 from flask import jsonify
+import json
 
 class Database:
     """
     This class gets the database instance, and exposes the collections.
     """
 
-    def __init__(self, uri = False):
+    def __init__(self, uri = False):        
         if type(uri) is str:
             self.client = MongoClient(uri)
         else:
@@ -25,3 +26,8 @@ class Database:
         Returns the User Database collection
         """
         return self.db['user']
+
+    def insertSW(self, software):
+        coll = self.db.list
+        coll.insert(software).inserted_id
+
