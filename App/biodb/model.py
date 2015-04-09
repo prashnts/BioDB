acad9cdefc845dc5b3fe9422c0bcf2a58f3b3eda
Prashant Sinha <prashnts@live.com>
2015-04-09 16:03:23 +0530

    Fixes

--- App/biodb/model.py
+++ App/biodb/model.py
@@ -2,3 +2,28 @@
 # -*- coding: utf-8 -*-
 #.--. .-. ... .... -. - ... .-.-.- .. -.
 
+# Local imports
+import utils
+
+class Software(object):
+    def add(software_name, tags, primary_link, one_liner, paid, primary_ref, remarks, meta = None):
+        pass
+
+    def delete(software_id):
+        pass
+
+    def update(
+            software_id,
+            _software_name = None,
+            _primary_link = None,
+            _one_liner = None,
+            _paid = None,
+            _primary_ref = None,
+            _remarks = None,
+            _meta = None
+        ):
+        pass
+
+class Feed(object):
+    def create(tags):
+        pass
