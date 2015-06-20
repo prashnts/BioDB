28a3f52ff33881bf19be4c2338f09b230eec3875
Prashant Sinha <prashnts@live.com>
2015-06-20 23:49:13 +0530

    Added admin template static files

--- App/app.py
+++ App/app.py
@@ -16,6 +16,7 @@ def addPage():
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	return render_template('index.html')
-	
+
+
 if __name__ == "__main__":
-    app.run(host = config['HOST'], port = config['PORT'], debug = config['DEBUG'])
+    app.run(host = config['HOST'], port = config['PORT'])
