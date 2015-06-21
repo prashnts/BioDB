09ed93c39d52d36ca154fa1288b917ff08b9cd20
Prashant Sinha <prashnts@live.com>
2015-06-21 10:56:49 +0530

    Few fixes and init changes

--- App/config.py
+++ App/config.py
@@ -2,7 +2,7 @@ import os
 
 config = {
     'PORT': os.getenv('OPENSHIFT_NODEJS_PORT', 60),
-    'HOST': os.getenv('OPENSHIFT_NODEJS_IP', 'localhost'),
+    'HOST': os.getenv('OPENSHIFT_NODEJS_IP', '0.0.0.0'),
     'MONGODB_URL':  os.getenv('OPENSHIFT_MONGODB_DB_URL', 'mongodb://localhost:27017/'),
     'Database': "softwares",
     'DEBUG': True
