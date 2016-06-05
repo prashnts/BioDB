15d866f39ebad4aeea016ad94c146708b5fd7b12
Prashant Sinha <prashant@ducic.ac.in>
2016-06-05 11:49:26 +0530

    + clear the junk

--- App/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-config = {
-    'PORT': os.getenv('OPENSHIFT_NODEJS_PORT', 9001),
-    'HOST': os.getenv('OPENSHIFT_NODEJS_IP', '0.0.0.0'),
-    'MONGODB_URL':  os.getenv('OPENSHIFT_MONGODB_DB_URL', 'mongodb://localhost:27017/'),
-    'Database': "softwares",
-    'DEBUG': True
-}
