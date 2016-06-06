80947fc059db9fc32b159763c82993ae698e610a
Prashant Sinha <prashant@ducic.ac.in>
2016-06-06 15:40:58 +0530

    + updated build conf

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-echo "${$(cat igloo/_config.sample.py)/\$module_dir/$(pwd)}" >! igloo/_config.py
-npm install
-npm run firstrun
--- igloo/_config.py
+++ igloo/_config.py
@@ -1,5 +1,6 @@
 # BioDB API
+import os
 
 db_name = 'bio.db'
-module_root = '$module_dir'
+module_root = os.getcwd()
 static_root = module_root + '/public'
